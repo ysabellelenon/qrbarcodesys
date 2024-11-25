88f6d6b0474b22a2c1ed8e76f0e81dbfd2823338
Ricky Lenon <rickylenon@gmail.com>
2024-11-26 07:46:20 +0800

    Base layout and bootstrap applied to pages

--- qr-barcode-frontend/sw.js
+++ qr-barcode-frontend/sw.js
@@ -14,6 +14,7 @@ const URLS_TO_CACHE = {
     pages: [
         '/login',
         '/engineering',
+        '/item-masterlist',
         '/assembly-login',
         '/assembly',
         '/offline.html'
